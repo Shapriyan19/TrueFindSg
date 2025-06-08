ca0bb13aefa95b4f992da0133cfc061719502875
Shapriyan19 <priyanmartian@gmail.com>
2025-06-08 13:00:44 +0530

    authentication

--- backend/api/routes/__init__.py
+++ backend/api/routes/__init__.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from api.controllers import firebase_controller
+from api.controllers import firebase_controller, auth_controller
 
 urlpatterns = [
-    path('firebase/',firebase_controller.home),
+    path('firebase/', firebase_controller.home),
+    path('auth/google/', auth_controller.GoogleSignInView.as_view(), name='google-signin'),
 ]
--- backend/users/urls.py
+++
@@ -1,8 +0,0 @@
-# users/urls.py
-from django.urls import path
-from .views import VerifiedProductsView, WatchlistView
-
-urlpatterns = [
-    path('verified/', VerifiedProductsView.as_view()),
-    path('watchlist/', WatchlistView.as_view()),
-]
