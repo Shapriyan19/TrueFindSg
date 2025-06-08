edd3070c3960f064087760f47b400eb3c0fc8b64
Shapriyan19 <priyanmartian@gmail.com>
2025-06-08 14:55:00 +0530

    changes

--- backend/api/routes/__init__.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from api.controllers import firebase_controller, auth_controller
-
-urlpatterns = [
-    path('firebase/', firebase_controller.home),
-    path('auth/google/', auth_controller.GoogleSignInView.as_view(), name='google-signin'),
-]
